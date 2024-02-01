ac917307099f472874576f1585e4babc9798a146
Elpastore <abdoulayesadio1997@gmail.com>
2024-02-01 15:51:29 -0800

    task1 pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,6 +10,7 @@ env.hosts = ["18.234.105.201", "100.26.162.114"]
 env.user = "ubuntu"
 env.key_filename = '/AirBnB_clone_v2/my_key'
 
+
 def do_pack():
     """
     method that generate a .tgz archive
@@ -26,11 +27,12 @@ def do_pack():
     except CommandFailed:
         return None
 
+
 def do_deploy(archive_path):
     """
     method for deploy web static into servers
     """
-    if  os.path.isfile(archive_path) is False:
+    if os.path.isfile(archive_path) is False:
         print("failed file doesn't exist")
         return False
 
@@ -38,42 +40,42 @@ def do_deploy(archive_path):
     new_path = archive_path.split('/')[-1]
     # Get the name of the archive without extension
     folder = new_path.split('.')[0]
-    
-    # upload the archive 
-    if put(archive_path, "/tmp/{}".format(new_path)).failed is True:
-        print("upload  failed")
-        return  False
-    # Creating the right path
-    if run("rm -rf /data/web_static/releases/{}/".format(folder)).failed is True:
+
+    # upload the archive
+    if put(archive_path, "/tmp/{}".
+            format(new_path)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".format(folder)).failed is True:
+    # Creating the right path
+    if run("rm -rf /data/web_static/releases/{}/".
+            format(folder)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+            format(folder)).failed is True:
         return False
     # uncompress
-    if run ("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(new_path, folder)).failed is True:
-        print("uncompressing failed")
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(new_path, folder)).failed is True:
         return False
     # remove the archive
-    if run ("rm  /tmp/{}".format(new_path)).failed is True:
-        print("removing the archive")
+    if run("rm  /tmp/{}".format(new_path)).failed is True:
         return False
     # move the content of web_static from uncompress folder
-    if run ("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(folder, folder)).failed is True:
-        print("moving file  to production directory failed")
+    if run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}".
+            format(folder, folder)).failed is True:
         return False
 
     # delete the symbolic link
-    if run ("rm -rf /data/web_static/current").failed is True:
-        print("deletion the symbolic link failed")
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
     # Remove content of web_static from uncompress folder
-    if run ("rm -rf /data/web_static/releases/{}/web_static".format(folder)).failed is True:
-        print("removing conten of web_static failed")
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+            format(folder)).failed is True:
         return False
     # create a new symoblic link
-    if run ("ln -sf /data/web_static/releases/{} /data/web_static/current".format(folder)).failed is True:
-        print("creating new symbolic link failed")
+    if run("ln -sf /data/web_static/releases/{} /data/web_static/current".
+            format(folder)).failed is True:
         return False
-    
-    print("New version deployed!")
-    return  True
 
+    print("New version deployed!")
+    return True
