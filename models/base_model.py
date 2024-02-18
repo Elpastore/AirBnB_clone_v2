1265a7d20fa077f09b38f9229223956e3a80ab2f
elpastore <abdoulayesadio1997@gmail.com>
2024-02-18 18:27:44 +0000

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
 
-
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
