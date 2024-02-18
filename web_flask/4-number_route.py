654c2d715fc97c0a11b71d10b17cdd69f609f2e3
elpastore <abdoulayesadio1997@gmail.com>
2024-02-18 11:30:54 +0000

    task4ÃÃ_v2

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -35,7 +35,7 @@ def python(text='is cool'):
 @app.route('/number', strict_slashes=False)
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return f'Python {n}'
+    return f'{n} is a number'
 
 
 if __name__ == '__main__':
